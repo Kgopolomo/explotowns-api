12bd9934c3165c6174eeb6a68f125bedf6e990ab
Kgopolomo <Kgopolomo@gmail.com>
2023-10-17 11:25:48 +0200

    initial commit

---
+++ src/main/java/com/explotwons/api/ExploTownApiApplication.java
@@ -0,0 +1,13 @@
+package com.explotwons.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploTownApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploTownApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explotwons/api/ExploTownApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explotwons.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploTownApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
